3b24821b2335ae06e56cf3a54691491ecd639a17
zgwiazda <s26615@pjwstk.edu.pl>
2024-05-28 14:46:03 +0200

    Initial commit

---
+++ WebApplication1/Controllers/TripsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
+
+namespace WebApplication1.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TripsController : ControllerBase
+{
+    private readonly ApbdContext _context;
+
+    public TripsController(ApbdContext context)
+    {
+        _context = context;
+    }
+    public async Task<IActionResult> GetTrips()
+    {
+
+        var context = new ApbdContext();
+        return Ok();
+    }
+    
+}
